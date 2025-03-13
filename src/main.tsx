ed213bbe7f3ed252c463ab50e2a60cf40ae9a2e6
Oibek Khashimov <o.khashimov@efg.gg>
2025-03-14 01:05:28 +0600

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Create from './Create.tsx'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <Router basename="/thumbups-webapp">
     <Routes>
-      <Route path="search" element={<App/>} />
-      <Route path="create" element={<Create/>} />
+      <Route path="/" element={<Navigate to="/search" replace />} />
+      <Route path="/search" element={<App/>} />
+      <Route path="/create" element={<Create/>} />
     </Routes>
   </Router>
 )
