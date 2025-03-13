8b810b3021a729c7a69867b55934915be7d2a324
Oibek Khashimov <o.khashimov@efg.gg>
2025-03-14 00:34:11 +0600

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Create from './Create.tsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <Router>
+  <BrowserRouter basename="/thumbups-webapp">
     <Routes>
-      <Route path="/thumbups-webapp/search" element={<App/>} />
-      <Route path="/thumbups-webapp/create" element={<Create/>} />
+      <Route path="/search" element={<App/>} />
+      <Route path="/create" element={<Create/>} />
     </Routes>
-  </Router>
+  </BrowserRouter>
 )
