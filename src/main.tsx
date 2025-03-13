6945c873fea6533df5c4407bc4989741a2cac130
Oibek Khashimov <o.khashimov@efg.gg>
2025-03-14 00:42:41 +0600

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Create from './Create.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/thumbups-webapp">
+  <Router basename="/thumbups-webapp">
     <Routes>
       <Route path="search" element={<App/>} />
       <Route path="create" element={<Create/>} />
     </Routes>
-  </BrowserRouter>
+  </Router>
 )
