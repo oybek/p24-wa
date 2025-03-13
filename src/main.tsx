d1ef75b000229603d7706f6d3f99193ad8160e33
Oibek Khashimov <o.khashimov@efg.gg>
2025-03-14 01:08:35 +0600

    last try for today

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Create from './Create.tsx'
-import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <Router>
+  <Router basename="/thumbups-webapp">
     <Routes>
+      {/* Redirect root to /search */}
       <Route path="/" element={<Navigate to="/search" replace />} />
-      <Route path="/search" element={<App/>} />
-      <Route path="/create" element={<Create/>} />
+      <Route path="/search" element={<App />} />
+      <Route path="/create" element={<Create />} />
     </Routes>
   </Router>
 )
