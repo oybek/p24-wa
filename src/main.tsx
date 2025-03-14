e1dbd3aa04b7d4d32969227f9740014368169e23
Oibek Khashimov <o.khashimov@efg.gg>
2025-03-14 10:31:23 +0600

    user_type parameter

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Create from './Create.tsx'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function UserTypeRouter() {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const userType = queryParams.get('user_type');
+
+  if (userType === 'driver') {
+    return <App />;
+  } else if (userType === 'user') {
+    return <Create />;
+  } else {
+    return <div>404</div>;
+  }
+}
 
 createRoot(document.getElementById('root')!).render(
   <Router basename="/thumbups-webapp">
     <Routes>
-      {/* Redirect root to /search */}
-      <Route path="/" element={<Navigate to="/search" replace />} />
-      <Route path="/search" element={<App />} />
-      <Route path="/create" element={<Create />} />
+      <Route path="/" element={<UserTypeRouter />} />
     </Routes>
   </Router>
 )
